fa8509182765f895b667496ef736230b2b8f4c58
bshap93 <benihana858@gmail.com>
2016-06-03 10:36:20 -0600

    NewGame class

--- lib/player.rb
+++ lib/player.rb
@@ -3,10 +3,9 @@ require './config/environment.rb'
 class Player
 
   attr_reader :token, :game
-
   def initialize(token, num_players)
     token == "X" || token == "O" ? @token = token : raise("Must select X or O")
-    self.game = NewGame.new(num_players)
+    @game = NewGame.which_game(num_players)
   end
 
 end
